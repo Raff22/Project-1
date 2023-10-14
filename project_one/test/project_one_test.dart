f23f06a13b7f186e3eefd51fae2eef0b383f4b93
Raffal Abu Eshey <86999942+Raff22@users.noreply.github.com>
2023-10-14 23:56:01 +0300

    adding test file

--- project_one/test/project_one_test.dart
+++ project_one/test/project_one_test.dart
@@ -1,8 +1,30 @@
-import 'package:project_one/project_one.dart';
-import 'package:test/test.dart';
+import 'package:project_one/book.dart';
+import 'package:project_one/bookstore.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  Book book = Book(title: 'Cooking', author: 'cheif', category: 'cooking');
+  Bookstore store = Bookstore();
+  test('getSize', () {
+    expect(store.getSize(), 9);
+  });
+  test('getAuthor', () {
+    expect(book.getAuthor(), 'cheif');
+  });
+  test('getTitle', () {
+    expect(book.getTitle(), 'Cooking');
+  });
+  test('getCategory', () {
+    expect(book.getCategory(), 'cooking');
+  });
+  test('getCopies', () {
+    expect(book.getCopies(), 10);
+  });
+  test('getPublisher', () {
+    expect(book.getPublisher(), null);
+  });
+  test('getPrice', () {
+    expect(book.getPrice(), 0.0);
   });
 }
